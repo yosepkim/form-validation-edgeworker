f8d96264dd31d6982d657618b8d1c3bb1087778c
Yosep Kim <yosepk@gmail.com>
2023-11-06 10:13:02 -0500

    adds github pipeline shell script

---
+++ azure-pipeline/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Download akamai CLI
+echo 'Installing akamai CLI'
+curl -o akamai https://github.com/akamai/cli/releases/download/v1.5.5/akamai-v1.5.5-linuxamd64
+chmod -x akamai
+
+echo 'Setting up .edgerc'
+echo -e "[default]\nhost=$(AKAMAI_HOST)\nclient_token=$(AKAMAI_CLIENT_TOKEN)\naccess_token=$(AKAMAI_ACCESS_TOKEN)\nclient_secret=$(AKAMAI_CLIENT_SECRET)" > ~/.edgerc
+
+echo 'Setting up EdgeKV token file'
+sed -e 's/${EDGEKV_TOKEN}/$(AKAMAI_EDGE_TOKEN)/g' ./edgekv_tokens.template.js > ./edgekv_tokens.js
+
+echo 'Building a deployment tarball'
+tar cvzf tarball.tgz main.js bundle.json edgekv.js edgekv_tokens.js services
+
+echo 'Deploying EdgeWorker ${TARGET_ENV}'
+akamai --accountkey $(ACCOUNT_KEY) edgeworkers upload --bundle tarball.tgz $(EW_ID)
